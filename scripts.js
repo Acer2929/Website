cd0927e31bffed56366433d6489679f7c07c3777
Mohit Bishnoi <146185710+Acer2929@users.noreply.github.com>
2024-09-06 11:30:27 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const img = document.querySelector("header img");
-    img.addEventListener("load", function() {
-        img.style.opacity = "1";
-    });
-});
